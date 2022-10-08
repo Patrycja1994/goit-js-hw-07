bd322b84278058851a797074fd11c9f7af84d993
Patrycja Lamik <patrycjalamik94@gmail.com>
2022-10-08 15:37:58 +0200

    zadanie 7 (1/2)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-const galleryList = document.querySelector(".gallery");
+const galleryList = document.querySelector("div.gallery");
 const elements = galleryItems
     .map( ({preview, original, description}) =>{
         return `<a class= "gallery_link" href= '${original}'>
@@ -15,3 +15,24 @@ const elements = galleryItems
     .join("");
 
 galleryList.insertAdjacentHTML("beforeend", elements);
+
+galleryList.addEventListener ("click", clickOpenPicture);
+function clickOpenPicture(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+instance.show()
+}
+
+galleryList.addEventListener("keydown", closePictures);
+function closePictures(event) {
+    if (event.code === 'ESCAPE') {
+        instance.close();
+    }
+}
+console.log(galleryItems);
